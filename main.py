c99804c8ed76a6595d55f42f71520137b4bd4bda
khushalid <64976580+khushalid@users.noreply.github.com>
2023-10-25 23:34:20 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 14:27:31 2023
+
+@author: tirthparekh
+"""
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Job Sraech Portal",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Job Search Portal! 👋")
+
+st.sidebar.success("Please select the demo needed from sidebar.")
